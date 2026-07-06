d7615d6a6d68f64e945347fe09c3ca6c605641b8
jasur784 <jasurabdullaev191@gmail.com>
2026-07-06 14:34:21 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart , Command 
+from aiogram.types import Message
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+Token = os.getenv("API")
+bot = Bot(token=Token)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer('Hello')
+    
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
